923740e19fdc20d0ada37f732c29b8033a27797e
Sumit Rakshit <sumitkumarrakshit@gmail.com>
2020-06-10 00:34:11 +0530

    Run webpack babel for server and use build

---
+++ config/webpack.server.js
@@ -0,0 +1,22 @@
+const path = require("path");
+
+module.exports = {
+  target: "node", // Server side. Not for client browser
+  entry: "./server/index.js",
+  output: {
+    filename: "s-bundle.js",
+    path: path.resolve(__dirname, "../build"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        options: {
+          presets: ["@babel/react", "@babel/env"],
+        },
+      },
+    ],
+  },
+};
--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const React = require("react");
 
+const renderToString = require("react-dom/server").renderToString;
+const AppComponent = require("./../src/App").default;
 
-app.get('/', (req,res)=> {
-    res.send('Hello Response');
-})
+app.get("/", (req, res) => {
+  const content = renderToString(<AppComponent />);
+  res.send(content);
+});
 
 app.listen(4000, () => {
-    console.log('Listening to Port 4000');
-  });
+  console.log("Listening to Port 4000");
+});
